acc17e5fe0bb849edc61d38d9ba6f9862033ecd2
MoiPad92 <moipad85@gmail.com>
2024-02-07 08:41:20 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio_Interfaces/IEnrutador.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Ejercicio_Interfaces
+{
+    internal interface IEnrutador
+    {
+        public void Imprimir();
+
+        public void ValidarIP();
+    }
+}
---
+++ Ejercicio_Interfaces/Router.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Ejercicio_Interfaces
+{
+    internal class Router : IEnrutador
+    {
+        public void Imprimir()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ValidarIP()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
